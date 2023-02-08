a889fadc84ef9beba4d921cfcc01023799e9587e
darshanjoshi9801 <darshan.joshi@marutitech.com>
2023-02-08 14:31:23 +0530

    Initial comit

---
+++ assn2_flask/web_app.py
@@ -0,0 +1,26 @@
+from flask import *
+from zipfile import ZipFile
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def file_upload():
+    return render_template('index.html')
+
+
+@app.route('/success' , methods=['GET','POST'])
+def success():
+    if request.method == 'POST':
+        file = request.files['zipfile']
+        with ZipFile(file ,'r') as zip_file:
+            zip_file.extractall()
+    return "zip file extracted successfully"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
