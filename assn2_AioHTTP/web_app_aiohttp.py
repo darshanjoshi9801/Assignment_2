4fb7fcb5027c133eb73d04a3a1937fbcb6a85919
darshanjoshi9801 <darshan.joshi@marutitech.com>
2023-02-23 12:07:02 +0530

    Added html files and enabled file downloading

--- assn2_AioHTTP/web_app_aiohttp.py
+++ assn2_AioHTTP/web_app_aiohttp.py
@@ -1,40 +1,41 @@
 import zipfile
 from aiohttp import web
+import jinja2
+import aiohttp_jinja2
+import os
+import aiohttp
 
+app = web.Application()
+aiohttp_jinja2.setup(
+    app, loader=jinja2.FileSystemLoader(os.path.join(os.getcwd(), "Templates"))
+)
 
-async def index():
-    return web.Response(text=
-    """
-    <!DOCTYPE html>
-    <head>
-    <title>Web app for extracting zip files</title>
-    </head>
-    <body>
-    <form action="/extract" enctype="multipart/form-data" method="post">
-    <input type="file" name="zip_file">
-    <input type="submit">
-    </form>
-    </body>
-    </html>
-    """, content_type='text/html')
+async def index(request):
+    response = aiohttp_jinja2.render_template("index.html", request, context={})
+    return response
 
 
 async def extract(request):
     data = await request.post()
     zip_file = data['zip_file'].file
+
     with zipfile.ZipFile(zip_file,'r') as z:
         extracted_files = z.namelist()
-        html = "<ul>"
-        for file in extracted_files:
-            html += f"<li><a href='/{file}' download>{file}</a></li>"
-        html += "</ul>"
-        return web.Response(text=html, content_type="text/html")
+        z.extractall(os.path.join(os.getcwd(), "extracted_files"))
 
+        context = {'file_names' : extracted_files, 'file_path' : os.path.join(os.getcwd(), "extracted_files")}
+        response = aiohttp_jinja2.render_template("response.html", request, context=context)
+        return response
 
-app = web.Application()
+async def handle_download(request):
+    file_name = request.match_info.get('file_name')
+    file_path = os.path.join(os.getcwd(), "extracted_files", file_name)
+    return web.FileResponse(file_path)
+
 app.add_routes([
     web.get('/', index),
-    web.post('/extract', extract)
+    web.post('/extract', extract),
+    web.get('/download/{file_name}',handle_download)
 ])
 if __name__ == '__main__':
     web.run_app(app)
